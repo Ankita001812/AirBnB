6f8a930b682d920f6e2640f6a89e6382eb011a28
Ankita001812 <s3980223@student.rmit.edu.au>
2025-06-14 16:47:36 +1000

    styled navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
+import Navbar from './components/Navbar';
 
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         {/* <Route path="/" element={<Home />} /> */}
         <Route path="/" element={<HomePage />} />
---
+++ front-end/src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../style/Navbar.css';
+
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo"><Link to='/' className="logo-link"><img src="/images/air.png" alt="AirBnB" /></Link></div>
+      <ul className="navbar-links">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/">Property Listing</Link>
+        </li>
+        <li>
+          <Link to="/booking">Bookings</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
